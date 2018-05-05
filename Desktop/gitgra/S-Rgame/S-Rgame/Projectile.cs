9b0c5b2068c09ccde55ffcd23f672e1aca56e6cd
SebastianGrodzki <sebastiangrodzki1@gmail.com>
2018-05-05 17:59:04 +0200

    enemy dmg player changes

--- Desktop/gitgra/S-Rgame/S-Rgame/Projectile.cs
+++ Desktop/gitgra/S-Rgame/S-Rgame/Projectile.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +8,20 @@ public class Projectile : MonoBehaviour
     public LayerMask collisionMask;
     float speed = 10;
     float damage = 1;
+
+    float lifetime = 3;
+    float skinWidth = .1f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+
+        Collider[] initialCollisions = Physics.OverlapSphere(transform.position, .1f, collisionMask);
+        if (initialCollisions.Length > 0)
+        {
+            OnHitObject(initialCollisions[0]);
+        }
+    }
 
     public void SetSpeed(float newSpeed)
     {
@@ -25,7 +39,7 @@ public class Projectile : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        if(Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        if(Physics.Raycast(ray, out hit, moveDistance + skinWidth, collisionMask, QueryTriggerInteraction.Collide))
         {
             OnHitObject(hit);
         }
@@ -36,6 +50,15 @@ public class Projectile : MonoBehaviour
         if (damageableObject != null)
         {
             damageableObject.TakeHit(damage, hit);
+        }
+        GameObject.Destroy(gameObject);
+    }
+    void OnHitObject(Collider c)
+    {
+        IDamageable damageableObject = c.GetComponent<IDamageable>(); 
+        if (damageableObject != null)
+        {
+            damageableObject.TakeDamage(damage);
         }
         GameObject.Destroy(gameObject);
     }
