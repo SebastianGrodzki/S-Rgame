c2e106651340bf5358fbb7874e11d3067ba36cb0
SebastianGrodzki <sebastiangrodzki1@gmail.com>
2018-05-21 22:03:30 +0200

    next step, next map
    g.o.

--- Desktop/gitgra/S-Rgame/S-Rgame/LivingEntity.cs
+++ Desktop/gitgra/S-Rgame/S-Rgame/LivingEntity.cs
@@ -28,6 +28,8 @@ public class LivingEntity : MonoBehaviour, IDamageable
             Die();
         }
     }
+
+    [ContextMenu("Self Destruct")]
     protected void Die()
     {
         dead = true;
