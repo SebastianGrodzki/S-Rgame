016efc661e75cc9c6c1ce31b2188a9b7ef62d51f
SebastianGrodzki <sebastiangrodzki1@gmail.com>
2018-06-01 00:20:37 +0200

    crosshairs

--- Desktop/gitgra/S-Rgame/S-Rgame/Player.cs
+++ Desktop/gitgra/S-Rgame/S-Rgame/Player.cs
@@ -9,6 +9,8 @@ public class Player : LivingEntity
 
     public float moveSpeed = 5;
 
+    public Crosshairs crosshairs;
+
     Camera viewCamera;
     PlayerController controller;
     GunController gunController;
@@ -30,7 +32,7 @@ public class Player : LivingEntity
 
         //look input
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.up * gunController.GunHeight);
         float rayDistance;
 
         if (groundPlane.Raycast(ray,out rayDistance))
@@ -38,6 +40,8 @@ public class Player : LivingEntity
             Vector3 point = ray.GetPoint(rayDistance);
             //Debug.DrawLine(ray.origin, point, Color.red);
             controller.LookAt(point);
+            crosshairs.transform.position = point;
+            crosshairs.DetectTargets(ray);
         }
         // weapon input
         if (Input.GetMouseButton(0))
