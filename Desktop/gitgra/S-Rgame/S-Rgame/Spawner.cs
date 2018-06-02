957836858c41681cd64d21aa5a2dc317dda3b960
SebastianGrodzki <sebastiangrodzki1@gmail.com>
2018-06-02 18:35:36 +0200

    sound fix

--- Desktop/gitgra/S-Rgame/S-Rgame/Spawner.cs
+++ Desktop/gitgra/S-Rgame/S-Rgame/Spawner.cs
@@ -129,6 +129,10 @@ public class Spawner : MonoBehaviour
 
     void NextWave()
     {
+        if (currentWaveNumber > 0)
+        {
+            AudioManager.instance.PlaySound2D ("Level Complete");
+        }
         currentWaveNumber++;
 
 
