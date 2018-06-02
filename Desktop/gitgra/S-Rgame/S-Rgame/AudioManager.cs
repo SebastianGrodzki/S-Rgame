a896a98fac89eac1e219422ef2cb4cf1d1b4a29a
SebastianGrodzki <sebastiangrodzki1@gmail.com>
2018-06-02 12:48:38 +0200

    audio

---
+++ Desktop/gitgra/S-Rgame/S-Rgame/AudioManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private float masterVolumePercent = .2f;
+    private float sfxVolumePercent = 1;
+    private float musicVolumePercent = 1;
+
+    private AudioSource[] musicSources;
+    private int activeMusicSourceIndex;
+
+    public static AudioManager instance;
+
+    Transform audioListener;
+    Transform playerT;
+
+    void Awake()
+    {
+        instance = this;
+        musicSources = new AudioSource[2];
+        for (int i = 0; i < 2; i++)
+        {
+            GameObject newMusicSource = new GameObject("Music source" + (i + 1));
+            musicSources[i] = newMusicSource.AddComponent<AudioSource>();
+            newMusicSource.transform.parent = transform;
+        }
+
+        audioListener = FindObjectOfType<AudioListener>().transform;
+        playerT = FindObjectOfType<Player>().transform;
+    }
+
+    void Update()
+    {
+        if (playerT != null)
+        {
+            audioListener.position = playerT.position;
+        }
+    }
+
+    public void PlayMusic(AudioClip clip, float fadeDuration = 1)
+    {
+        activeMusicSourceIndex = 1 - activeMusicSourceIndex;
+        musicSources[activeMusicSourceIndex].clip = clip;
+        musicSources[activeMusicSourceIndex].Play();
+
+        StartCoroutine(AnimateMusicCrossfade(fadeDuration));
+    }
+
+    public void PlaySound(AudioClip clip, Vector3 pos)
+    {
+        if (clip != null)
+        {
+            AudioSource.PlayClipAtPoint(clip, pos, sfxVolumePercent * masterVolumePercent);
+        }
+    }
+
+    IEnumerator AnimateMusicCrossfade(float duration)
+    {
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime * 1 / duration;
+            musicSources[activeMusicSourceIndex].volume = Mathf.Lerp(0, musicVolumePercent * masterVolumePercent, percent);
+            musicSources[1-activeMusicSourceIndex].volume = Mathf.Lerp(musicVolumePercent * masterVolumePercent, 0, percent);//Max vol. music
+            yield return null;
+        }
+    }
+}
