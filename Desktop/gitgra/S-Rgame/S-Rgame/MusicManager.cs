f3eee8f8359f61ea40134e0342905c5391de0fe4
SebastianGrodzki <sebastiangrodzki1@gmail.com>
2018-06-06 00:24:53 +0200

    menu changes

--- Desktop/gitgra/S-Rgame/S-Rgame/MusicManager.cs
+++ Desktop/gitgra/S-Rgame/S-Rgame/MusicManager.cs
@@ -1,23 +1,56 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
     public AudioClip mainTheme;
     public AudioClip menuTheme;
 
+    string sceneName;
+
     void Start()
     {
-        AudioManager.instance.PlayMusic(menuTheme, 2);
+        OnLevelWasLoaded(0);
     }
 
 
-	void Update ()
-	{
-	    if (Input.GetKeyDown(KeyCode.Space))
-	    {
-	        AudioManager.instance.PlayMusic(mainTheme, 3);
-	    }
-	}
+    void OnLevelWasLoaded(int sceneIndex)
+    {
+        string newSceneName = SceneManager.GetActiveScene().name;
+        if (newSceneName != sceneName)
+        {
+            sceneName = newSceneName;
+            Invoke("PlayMusic", .2f);
+        }
+    }
+
+    void PlayMusic()
+    {
+        AudioClip clipToPlay = null;
+
+        if (sceneName == "Menu")
+        {
+            clipToPlay = menuTheme;
+        }
+        else if (sceneName == "Game")
+        {
+            clipToPlay = mainTheme;
+        }
+
+        if (clipToPlay != null)
+        {
+            AudioManager.instance.PlayMusic(clipToPlay, 2);
+            Invoke("PlayMusic", clipToPlay.length);
+        }
+    }
+
+	//void Update ()
+	//{
+	//    if (Input.GetKeyDown(KeyCode.Space))
+	//    {
+	//        AudioManager.instance.PlayMusic(mainTheme, 3);
+	//    }
+	//}
 }
