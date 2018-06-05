cc89b24e9b870a1aae2507ed6f8444d8853deffc
SebastianGrodzki <sebastiangrodzki1@gmail.com>
2018-06-06 00:24:23 +0200

    menu changes...

--- Desktop/gitgra/S-Rgame/S-Rgame/Player.cs
+++ Desktop/gitgra/S-Rgame/S-Rgame/Player.cs
@@ -75,6 +75,11 @@ public class Player : LivingEntity
         {
             gunController.Reload();
         }
+
+        if (transform.position.y < -10)
+        {
+            TakeDamage(health);
+        }
     }
 
     public override void Die()  //die sound
